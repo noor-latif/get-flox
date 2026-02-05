8c668e1e51fbcd9d17d9d43bdfe62ff5dd77bc34
Noor Latif <noor@latif.se>
2026-02-05 08:34:04 +0000

    remove: delete legacy nix-shell configuration

--- shell.nix
+++
@@ -1,8 +0,0 @@
-with (import <nixpkgs> {});
-mkShell {
-  buildInputs = [ bash curl wget gnupg coreutils which shellcheck ];
-  shellHook = ''
-    echo "Entering get-flox legacy nix-shell"
-    echo "Use: nix develop or nix-shell"
-  '';
-}
